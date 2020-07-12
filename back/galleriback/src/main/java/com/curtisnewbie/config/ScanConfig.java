c2a800cf0243326476b829f4c5b5c937a49302e3
CurtisNewbie <zhuangyongj@gmail.com>
2020-07-12 22:04:25 +0800

    Fix Configuration Properties Injection

--- back/galleriback/src/main/java/com/curtisnewbie/config/ScanConfig.java
+++ back/galleriback/src/main/java/com/curtisnewbie/config/ScanConfig.java
@@ -1,21 +1,44 @@
 package com.curtisnewbie.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * Configuration in {@code application.properties} for image scanning
  */
+@Component
 @ConfigurationProperties(prefix = "scan")
 public class ScanConfig {
 
     private String dir;
     private String defDir;
 
-    public String dir() {
+    /**
+     * @return the dir
+     */
+    public String getDir() {
         return dir;
     }
 
-    public String defaultDir() {
+    /**
+     * @param dir the dir to set
+     */
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    /**
+     * @return the default dir
+     */
+    public String getDefDir() {
         return defDir;
     }
+
+    /**
+     * @param defDir the defDir to set
+     */
+    public void setDefDir(String defDir) {
+        this.defDir = defDir;
+    }
+
 }

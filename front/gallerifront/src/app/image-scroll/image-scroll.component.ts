4f237f5c0a5923d8a8a827ee2d2a71088a4147f0
CurtisNewbie <zhuangyongj@gmail.com>
2020-07-17 21:52:06 +0800

    Improve UI for devices with smaller screen

--- front/gallerifront/src/app/image-scroll/image-scroll.component.ts
+++ front/gallerifront/src/app/image-scroll/image-scroll.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
 import { Image, toImageList } from '../../image';
 import { HttpService } from '../http.service';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+
+const MIN_ITEM_HEIGHT = 300; // in pixel 
 
 @Component({
   selector: 'app-image-scroll',
@@ -8,12 +11,16 @@ import { HttpService } from '../http.service';
   styleUrls: ['./image-scroll.component.css']
 })
 export class ImageScrollComponent implements OnInit {
-
+  itemHeight: number = this.calcItemHeight();
   images: Image[] = [];
+
+  @ViewChild("virtualScroll", { static: true })
+  virtualScroll: CdkVirtualScrollViewport;
 
   constructor(private http: HttpService) { }
 
   ngOnInit() {
+    console.log("Item Height:", this.itemHeight);
     this.fetchAllImageUrls();
   }
 
@@ -23,5 +30,10 @@ export class ImageScrollComponent implements OnInit {
         this.images = toImageList(v);
       }
     });
+  }
+
+  private calcItemHeight(): number {
+    let height = Math.floor(window.innerHeight * 0.75);
+    return height < MIN_ITEM_HEIGHT ? MIN_ITEM_HEIGHT : height;
   }
 }
